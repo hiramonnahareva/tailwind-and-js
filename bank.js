daa1f2bcf8323bda43a9c822afe8a83669c12472
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2022-02-10 02:43:17 -0800

    add mainsection1

---
+++ bank.js
@@ -0,0 +1,5 @@
+document.getElementById('loginbtn').addEventListener('click' , function (){
+    const emailField = document.getElementById("user-email");
+    const useremail = emailField.value;
+    console.log (useremail);
+})
